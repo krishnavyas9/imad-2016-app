faec2665ffc5a2f415d2bfe01dcaa158c0896e86
krishnavyas9 <150120107186@git.org.in>
2016-10-26 14:21:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,8 +12,9 @@ app.get('/article-one', function (req, res) {
       res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 
 });
-app.get('/article-two', function (req, res) {
-    res.send('article two requested and will be served here');
+app.get('/videos', function (req, res) {
+      res.sendFile(path.join(__dirname, 'ui', 'videos.html'));
+
 });
 app.get('/article-three', function (req, res) {
     res.send('article three requested and will be served here');
